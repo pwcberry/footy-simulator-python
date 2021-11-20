85ec4184225f23943272be03d711d3a731ea5e42
Peter Berry <pwcberry@gmail.com>
2021-11-20 16:10:58 +1100

    Add ForwardMatrix class definition

--- matrix.py
+++ matrix.py
@@ -56,6 +56,16 @@ def prob(base, strength, accuracy, pressure):
     return value if value > 0 else 0
 
 
+def prob_dist(dist, base, strength, accuracy, pressure):
+    # Skill probability
+    p = prob(base, strength, accuracy, pressure)
+
+    # Distance probability
+    d = 1 - 0.4 * (dist - 1)
+
+    return p + d * p  
+
+
 def normalise(row, dynamic_indexes):   
     prob_sum = fsum(row)
 
@@ -88,55 +98,53 @@ class MidFieldMatrix:
                     0, 0.05, 0.05, 0.01, 
                     prob(0.05, hst, 0, ap), prob(0.02, 0, -ha, -ap), 0.01, prob(0.2, hst, ha, ap), 
                     prob(0.05, ast, 0, hp), prob(0.02, 0, -aa, -hp), 0.1, prob(0.2, ast, aa, hp)
-                ], [4, 5, 7, 8, 9, 11])
-            ),
+                ], [4, 5, 7, 8, 9, 11])),
             (RUCK_STOPPED_STATUS, [0.9, 0.03, 0.07, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
             (RUCK_THROW_IN_STATUS, 
                 normalise([
                     0, 0.05, 0, 0.08,
                     prob(0.06, hst, 0, ap), prob(0.01, 0, -ha, -ap), 0.01, prob(0.25, hst, ha, ap),
                     prob(0.06, ast, 0, hp), prob(0.01, 0, -aa, -hp), 0.01, prob(0.25, ast, aa, hp)
-                ], [4, 5, 7, 8, 9, 11]),
+                ], [4, 5, 7, 8, 9, 11])),
             (MID_FIELD_THROW_IN_STATUS, [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
             (MID_FIELD_HOME_TEAM_STOPPED_STATUS,
                 normalise([
                     0, 0, 0, 0.01,
                     prob(0.1, hst, 0, ap), 0, prob(0.05, hst, 0, 0), prob(0.44, hst, ha, ap),
                     prob(0.08, ast, 0, hp), 0, prob(0.02, 0, 0, -ap), prob(0.02, ast, 0, -ap)
-                ]), [4, 6, 7, 8, 10, 11]),
+                ], [4, 6, 7, 8, 10, 11])),
             (MID_FIELD_HOME_TEAM_OUT_OF_BOUNDS_STATUS, [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]),
             (MID_FIELD_HOME_TEAM_FREE_KICK_STATUS,
                 normalise([
                     0, 0, 0, 0, 
                     prob(0.05, 0, -ha, -hp), prob(0.05, 0, -ha, -ap), prob(0.01, 0, 0, -hp), prob(0.55, hst, ha, ap),
                     prob(0.08, 0, -ha, -ap), 0, prob(0.015, 0, 0, -ap), 0
-                ]), [4, 5, 6, 7, 8, 10]),
+                ], [4, 5, 6, 7, 8, 10])),
             (MID_FIELD_HOME_TEAM_MOVING_STATUS,
                 normalise([
                     0, 0, 0, prob(0.025, 0, -ha, -ap),
                     prob(0.03, 0, -aa, -ap), prob(0.025, 0, -ha, -ap), prob(0.04, hst, 0, -hp), prob(0.5, hst, ha, ap),
                     prob(0.03, 0, -ha, -ap), prob(0.025, 0, -aa, -hp), prob(0.03, ast, 0, -ap), 0
-                ]), [3, 4, 5, 6, 7, 8, 9, 10]),
+                ], [3, 4, 5, 6, 7, 8, 9, 10])),
             (MID_FIELD_AWAY_TEAM_STOPPED_STATUS,
                 normalise([
                     0, 0, 0, 0.01,
                     prob(0.08, hst, 0, ap), 0, prob(0.02, 0, 0, -hp), prob(0.02, hst, 0, hp), 
                     prob(0.1, ast, 0, hp), 0, prob(0.05, ast, 0, 0), prob(0.44, ast, aa, hp)
-                ]), [4, 6, 7, 8, 10, 11]),
+                ], [4, 6, 7, 8, 10, 11])),
             (MID_FIELD_AWAY_TEAM_OUT_OF_BOUNDS_STATUS, [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]),
             (MID_FIELD_AWAY_TEAM_FREE_KICK_STATUS, 
                 normalise([
                     0, 0, 0, 0,
                     prob(0.08, 0, -aa, -hp), 0, prob(0.015, 0, 0, -hp), 0,
                     prob(0.05, 0, -aa, -ap), prob(0.05, 0, -aa, hp), prob(0.01, 0, 0, -ap), prob(0.55, ast, aa, hp)
-                ]), [4, 6, 8, 9, 10, 11]),
+                ], [4, 6, 8, 9, 10, 11])),
             (MID_FIELD_AWAY_TEAM_MOVING_STATUS, 
                 normalise([
                     0, 0, 0, prob(0.025, 0, -aa, -hp),
                     prob(0.03, 0, -aa, -hp), prob(0.025, 0, -ha, -ap), prob(0.03, hst, 0, -hp), 0,
                     prob(0.03, 0, -ha, -ap), prob(0.025, 0, -aa, -hp), prob(0.04, ast, 0, -hp), prob(0.5, ast, aa, hp)
-                ]), [3, 4, 5, 6, 8, 9, 10, 11])
-            )
+                ], [3, 4, 5, 6, 8, 9, 10, 11]))
         )
 
     @property
@@ -144,7 +152,7 @@ class MidFieldMatrix:
         return self.matrix.keys()
 
 class ForwardMatrix:
-    def __init__(self, home_team_skill, away_team_skill, distance):
+    def __init__(self, home_team_skill, away_team_skill, dist):
         hst = home_team_skill.strength
         ha = home_team_skill.accuracy
         hp = home_team_skill.pressure
@@ -152,22 +160,73 @@ class ForwardMatrix:
         aa = away_team_skill.accuracy
         ap = away_team_skill.pressure
 
-        # Distance probability where distance is 1, 2, or 3
-        dp = 0.5 + (2 - distance) * 0.3
-
-
         self.matrix = dict(
-            (FORWARDS_THROW_IN_STATUS, [], [])
-            (FORWARDS_HOME_TEAM_STOPPED_STATUS, [], [])
-            (FORWARDS_HOME_TEAM_OUT_OF_BOUNDS_STATUS, [], []),
-            (FORWARDS_HOME_TEAM_FREE_KICK_STATUS, [], []),
-            (FORWARDS_HOME_TEAM_MOVING_STATUS, [], []),
-            (FORWARDS_HOME_TEAM_BEHIND_STATUS, [], []),
-            (FORWARDS_HOME_TEAM_GOAL_STATUS, [], []),
-            (FORWARDS_AWAY_TEAM_STOPPED_STATUS, [], []),
-            (FORWARDS_AWAY_TEAM_OUT_OF_BOUNDS_STATUS, [], []),
-            (FORWARDS_AWAY_TEAM_FREE_KICK_STATUS, [], []),
-            (FORWARDS_AWAY_TEAM_MOVING_STATUS, [], [])
+            (RUCK_BOUNCE_STATUS, 
+                normalise([
+                    0, 0.05, 0.045, 0.005, 
+                    prob(0.05, hst, 0, ap), prob(0.02, 0, -ha, -ap), 0.01, prob(0.25, hst, ha, ap),
+                    prob_dist(dist, 0.005, hst, 0, ap), prob_dist(dist, 0.005, hst, ha, ap),
+                    prob(0.06, ast, 0, hp), prob(0.01, 0, -aa, -hp), 0.01, prob(0.25, ast, aa, hp) 
+                ], [4, 5, 7, 8, 9, 10, 11, 13])),
+            (RUCK_STOPPED_STATUS, [0.9, 0.03, 0.07, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+            (RUCK_THROW_IN_STATUS, 
+                normalise([
+                    0, 0.05, 0, 0.065,
+                    prob(0.06, hst, 0, ap), prob(0.01, 0, -ha, -ap), 0.01, prob(0.25, hst, ha, ap),
+                    prob_dist(dist, 0.005, hst, 0, ap), prob_dist(dist, 0.005, hst, ha, ap),
+                    prob(0.06, ast, 0, hp), prob(0.01, 0, -aa, -hp), 0.01, prob(0.25, ast, aa, hp)
+                ], [4, 5, 7, 8, 9, 11])),
+            (FORWARDS_THROW_IN_STATUS, 
+                normalise(
+                    [0, 0, 0.088, 0, 
+                     0, 0, 0, 0, 
+                     prob_dist(dist, 0.005, hst, 0, ap), prob_dist(dist, 0.005, hst, ha, ap), 
+                     0, 0, 0, 0], [8, 9])),
+            (FORWARDS_HOME_TEAM_STOPPED_STATUS, 
+                normalise([
+                    0, 0, 0, 0.01,
+                    prob(0.1, hst, 0, ap), 0, prob(0.05, hst, 0, 0), prob(0.44, hst, ha, ap),
+                    0, 0,
+                    prob(0.08, ast, 0, hp), 0, prob(0.02, 0, 0, -ap), prob(0.02, ast, 0, -ap)
+                ], [4, 6, 7, 10, 12, 13])),
+            (FORWARDS_HOME_TEAM_OUT_OF_BOUNDS_STATUS, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]),
+            (FORWARDS_HOME_TEAM_FREE_KICK_STATUS,
+                normalise([
+                    0, 0, 0, 0, 
+                    prob(0.04, 0, -ha, -hp), prob(0.04, 0, -ha, -ap), prob(0.01, 0, 0, -hp), prob(0.52, hst, ha, ap),
+                    prob_dist(dist, 0.05, 0, ha, ap), prob_dist(dist, 0.07, hst, ha, ap),
+                    prob(0.04, 0, -ha, -ap), 0, prob(0.005, 0, 0, -ap), 0
+                ], [4, 5, 6, 7, 8, 9, 10, 12])),
+            (FORWARDS_HOME_TEAM_MOVING_STATUS, normalise([
+                    0, 0, 0, prob(0.02, 0, -ha, -ap),
+                    prob(0.02, 0, -aa, -ap), prob(0.015, 0, -ha, -ap), prob(0.03, hst, 0, -hp), prob(0.2, hst, ha, ap),
+                    prob_dist(dist, 0.15, 0, ha, ap), prob_dist(dist, 0.18, hst, ha, ap),
+                    prob(0.03, 0, -ha, -ap), prob(0.01, 0, -aa, -hp), prob(0.01, ast, 0, -ap), 0
+                ], [3, 4, 5, 6, 7, 8, 9, 10, 11, 12])),
+            (FORWARDS_HOME_TEAM_BEHIND_STATUS, [0, 0, 0, 0, 0, 0, 0, 1, 0, 0,  0, 0, 0, 0]),
+            (FORWARDS_HOME_TEAM_GOAL_STATUS, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+            (FORWARDS_AWAY_TEAM_STOPPED_STATUS, 
+                normalise([
+                    0, 0, 0, 0.01,
+                    prob(0.08, hst, 0, ap), 0, prob(0.02, 0, 0, -hp), prob(0.02, hst, 0, hp),
+                    0,  0,
+                    prob(0.1, ast, 0, hp), 0, prob(0.05, ast, 0, 0), prob(0.44, ast, aa, hp)
+                ], [4, 6, 7, 10, 12, 13])),
+            (FORWARDS_AWAY_TEAM_OUT_OF_BOUNDS_STATUS, [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+            (FORWARDS_AWAY_TEAM_FREE_KICK_STATUS, 
+                normalise([
+                    0, 0, 0, 0,
+                    prob(0.08, 0, -aa, -hp), 0, prob(0.015, 0, 0, -hp), 0,
+                    0, 0,
+                    prob(0.05, 0, -aa, -ap), prob(0.05, 0, -aa, hp), prob(0.01, 0, 0, -ap), prob(0.55, ast, aa, hp)
+                ], [4, 6, 10, 11, 12, 13])),
+            (FORWARDS_AWAY_TEAM_MOVING_STATUS, 
+                normalise([
+                    0, 0, 0, prob(0.025, 0, -aa, -hp),
+                    prob(0.03, 0, -aa, -hp), prob(0.025, 0, -ha, -ap), prob(0.03, hst, 0, -hp), 0,
+                    0, 0,
+                    prob(0.03, 0, -ha, -ap), prob(0.025, 0, -aa, -hp), prob(0.04, ast, 0, -hp), prob(0.5, ast, aa, hp)
+                ], [3, 4, 5, 6, 10, 11, 12, 13]))
         )
   
     @property
